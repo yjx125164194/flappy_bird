9a2960bb5247f147f4732b67310e4a1e93b600c8
yjx <yjx@163.com>
2017-08-10 10:18:35 +0800

    test shift

--- bird.c
+++ bird.c
@@ -8,8 +8,9 @@
 #define pillar    2
 #define bird      3
 
-#define wall_wide   3
-#define pillar_wide 5
+#define wall_wide     3
+#define pillar_wide   5
+#define pillar_length 5
 
 #define LENGTH    100
 #define WIDE      20
@@ -20,6 +21,8 @@ struct Block
     int content;
 }block[WIDE][LENGTH];
 
+static int count = 0;
+
 void init_block(struct Block fp[WIDE][LENGTH])
 {
   int i,j;
@@ -27,18 +30,50 @@ void init_block(struct Block fp[WIDE][LENGTH])
   {
     for(j = 0;j < LENGTH;j++)
     {
-      fp[i][j].status = 0;
+      fp[i][j].status = false;
       if(i < wall_wide || i >=  WIDE - wall_wide)
       {
         fp[i][j].content = wall; 
+        fp[i][j].status = true;
       }
       else
       {
         fp[i][j].content = empty; 
+        fp[i][j].status = false;
       }
     }
   }
 }
+void shift_block(struct Block fp[WIDE][LENGTH])
+{
+  int i,j;
+
+  for(i = 0;i < WIDE;i++)
+  {
+    for(j = 0;j < LENGTH - 1;j++)
+    {
+      fp[i][j].content = fp[i][j+1].content;
+    }
+  }
+  
+  for(i = 0;i < WIDE;i++)
+  {
+    if(count%(pillar_length + 8) < pillar_length 
+      && (i >= wall_wide && i < wall_wide + pillar_wide)
+      && (i > WIDE - pillar_wide - wall_wide && i < WIDE - wall_wide) 
+      ) 
+    {
+      fp[i][LENGTH-1].content = pillar;
+      fp[i][LENGTH-1].status = true;
+    }
+    else
+    {
+      fp[i][LENGTH-1].content = empty;
+      fp[i][LENGTH-1].status = false;
+    }
+  }
+}
+
 
 void print_block(struct Block fp[WIDE][LENGTH])
 {
@@ -69,6 +104,7 @@ void print_block(struct Block fp[WIDE][LENGTH])
     }
     printf("\n");
   }
+  count++;
   sleep(1);
 }
 
@@ -79,6 +115,7 @@ int main(int argc,char ** argv)
   while(1)
   {
     print_block(block); 
+    shift_block(block);
   }
   return 0;
 }
