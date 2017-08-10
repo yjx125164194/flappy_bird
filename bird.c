0738db313482ecaab647e758a1071a308cb0ef89
yjx <yjx@163.com>
2017-08-10 15:03:46 +0800

    v2.0

--- bird.c
+++ bird.c
@@ -3,6 +3,8 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<termio.h>
+#include<unistd.h>
+#include<fcntl.h>
 
 #define empty     0
 #define wall      1
@@ -214,8 +216,10 @@ void print_block(struct Block fp[WIDE][LENGTH])
     printf("\n");
   }
   count++;
-  //sleep(1);
+  usleep(500000);
 }
+
+//windows can use getch()
 int sh_getch(void)
 {
   int cr;
@@ -237,11 +241,33 @@ int sh_getch(void)
 
   return cr; 
 }
+//windows can use kbhit
+int kbhit(void)
+{
+  struct termios oldt,newt;
+  int ch;
+  int oldf;
+  tcgetattr(STDIN_FILENO,&oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON|ECHO);
+  tcsetattr(STDIN_FILENO,TCSANOW,&newt);
+  oldf = fcntl(STDIN_FILENO,F_GETFL,0);
+  fcntl(STDIN_FILENO,F_SETFL, oldf | O_NONBLOCK);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO,TCSANOW,&oldt);  
+  fcntl(STDIN_FILENO,F_SETFL,oldf);
+  if(ch != EOF)
+  {
+    ungetc(ch,stdin);
+    return 1;
+  }
+  return 0;
+}
 
 int main(int argc,char ** argv)
 {
   int direction;
-  char ch;
+  
   srand((unsigned int) time(0));
 
   init_block(block);
@@ -251,25 +277,32 @@ int main(int argc,char ** argv)
 
   while(1)
   {
-    switch(ch = sh_getch())
+    if(!kbhit())
     {
-      case '8':
-        direction = bird_up;
-        break;
-      case '4':
-        direction = bird_left;
-        break;
-      case '6':
-        direction = bird_right;
-        break;
-      case '5':
-        direction = bird_down;
-        break;
-      case 'q':
-        exit(0);
-      default:
-        direction = bird_down;
-        break;
+      direction = bird_down;
+    }
+    else
+    {
+      switch(sh_getch())
+      {
+        case '8':
+          direction = bird_up;
+          break;
+        case '4':
+          direction = bird_left;
+          break;
+        case '6':
+          direction = bird_right;
+          break;
+        case '5':
+          direction = bird_down;
+          break;
+        case 'q':
+          exit(0);
+       default:
+          direction = bird_down;
+          break;
+      }
     }
     setbuf(stdin,NULL);
     /*while(getchar() != '\n')
